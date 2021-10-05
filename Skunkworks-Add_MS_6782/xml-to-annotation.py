1fa61cfc9e91da804303a6cdc3d18faa73c15537
Don Brower <dbrower@nd.edu>
2021-10-05 10:53:04 -0400

    Get all the text() elements of each <s> tag
    Handle the case where a page has 0 or >1 <s> tags as well.

--- Skunkworks-Add_MS_6782/xml-to-annotation.py
+++ Skunkworks-Add_MS_6782/xml-to-annotation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Convert an ECHO XML file into a list of annotation JSON blobs.
 #
@@ -7,6 +7,12 @@
 import xml.etree.ElementTree as ET
 import json
 import sys
+
+# concat_all_texts takes an ElementTree element and returns all the text()
+# sections in it and its subelements as one big string.
+def concat_all_texts(element):
+    return "".join(element.itertext())
+
 
 ns = {
         "xsl": "http://www.w3.org/1999/XSL/Transform",
@@ -28,7 +34,18 @@ pages = data.findall(".//echo:div[@type='page']", ns)
 for page in pages:
     pb = page.find('echo:pb', ns)
     #user = page.find('./echo:c/@user', ns)
-    comment = page.findtext('.//echo:s', '', ns)
+
+    # Not every page has an <s></s> section. and some have more than one.
+    # So get all the <s> sections on this page. If there are none, make
+    # an empty annotation. If there is more than one, smush them all together
+    # into one annotation.
+    echo_all_s = page.findall('.//echo:s', ns)
+    comment = ""
+    if len(echo_all_s) > 0:
+        # smush all the s sections together
+        comment = " ".join([concat_all_texts(s) for s in echo_all_s])
+    # fix the text up so there is no leading or trailing whitespace and remove
+    # the newlines, which are not important for IIIF annotations.
     comment = comment.strip()
     comment = comment.replace('\n', ' ')
 
